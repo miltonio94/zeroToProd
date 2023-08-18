d7af2cf098d77cfa949e657ecb4c93454be220a0
Milton <miltonio94@gmail.com>
2023-08-18 08:18:43 +0000

    refactor to use PgPool instead of PgConnection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_config;
 use zero2prod::startup::run;
@@ -5,8 +6,11 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = get_config().expect("Failed to read configuration");
+    let connection = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Filed to connect to postgres");
     let address = format!("127.0.0.1:{}", config.app_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    run(listener, connection)?.await
 }
